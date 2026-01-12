1bbc08b497ee398fe7ac54f902accb08f61c5f3e
Волох Алексей Николаевич <Voloh.a@dns-shop.ru>
2026-01-12 18:14:44 +0600

    Reloaded

--- src/main/hibernate_test/Test1.java
+++ src/main/hibernate_test/Test1.java
@@ -5,8 +5,18 @@ import org.hibernate.cfg.Configuration;
 public class Test1 {
     public static void main(String[] args) {
 
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory()) {
+            Session session = factory.getCurrentSession();
 
+            Employee emp = new Employee("Maksim", "Ded", "Ogorod", 500);
 
+            session.beginTransaction();
+            session.persist(emp);
+            session.getTransaction().commit();
+        }
 
 
     }
