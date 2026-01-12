020945df8431c39602069704e92334dfe697d95f
Волох Алексей Николаевич <Voloh.a@dns-shop.ru>
2026-01-12 15:39:40 +0600

    Reloaded

---
+++ src/main/hibernate_test/Test1.java
@@ -0,0 +1,13 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
